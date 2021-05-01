7e7d3f43eb600566170807df8613bcfcbd576497
huss <hussein.odeh@gmail.com>
2021-05-01 10:57:12 +0200

    feat: added middleware morgan, cors, helmet

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,13 @@
 import express, { Application } from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+import morgan from 'morgan';
 import { routes } from './routes';
 
 export const app: Application = express();
+
+app.use(morgan('common'));
+app.use(helmet());
+app.use(cors());
 
 routes(app);
