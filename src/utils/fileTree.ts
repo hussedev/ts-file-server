e309f7a94893f0ec507f97ced0eecc6d8e157ec6
huss <hussein.odeh@gmail.com>
2021-05-02 22:18:38 +0200

    feat: client can choose what folder tree to get

--- src/utils/fileTree.ts
+++ src/utils/fileTree.ts
@@ -1,6 +1,6 @@
 import directoryTree, { DirectoryTree } from 'directory-tree';
 
-export async function dirTree(folder: string = ''): Promise<DirectoryTree> {
-  const tree = await directoryTree('./public');
+export async function dirTree(folder: string = '/public'): Promise<DirectoryTree> {
+  const tree = await directoryTree(`.${folder}`);
   return tree;
 }
