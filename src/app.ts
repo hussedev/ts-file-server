36aa316528c9584254c9fc52682a34650ddcca5c
huss <hussein.odeh@gmail.com>
2021-05-02 22:15:44 +0200

    feat: serves static files and parses request bodys

--- src/app.ts
+++ src/app.ts
@@ -9,5 +9,6 @@ export const app: Application = express();
 app.use(morgan('common'));
 app.use(helmet());
 app.use(cors());
-
+app.use('/file', express.static('public'));
+app.use(express.json());
 routes(app);
